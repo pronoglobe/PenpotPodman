7f4df00ddb6fb68067b51cc8afc0975c4c1acb24
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-10-20 08:51:32 +0100

    feat(run.sh): update container image references

--- run.sh
+++ run.sh
@@ -18,12 +18,13 @@ podman network create penpot
 
 podman pod create --replace --network penpot -p 127.0.0.1:9001:80 -p 127.0.0.1:1080:1080 penpot
 
-podman run --replace --name=penpot-mailcatch --expose=1025 --pod penpot -d sj26/mailcatcher:latest
-podman run --replace --name=penpot-redis --pod penpot -d redis:7
-podman run --replace --name=penpot-postgres --pod penpot --volume=./db:/var/lib/postgresql/data:rw --env-file=./pod.env -d postgres:15
+podman run --replace --name=penpot-mailcatch --expose=1025 --pod penpot -d docker.io/sj26/mailcatcher:latest
+podman run --replace --name=penpot-redis --pod penpot -d docker.io/library/redis:7
+podman run --replace --name=penpot-postgres --pod penpot --volume=./db:/var/lib/postgresql/data:rw --env-file=./pod.env -d docker.io/library/postgres:15
 
-# Uncommen when need be
-podman run --replace --name=penpot-exporter --pod penpot --env-file=./pod.env -d penpotapp/exporter:latest
+# Uncomment when needed
+podman run --replace --name=penpot-exporter --pod penpot --env-file=./pod.env -d docker.io/penpotapp/exporter:latest
 
-podman run --replace --name=penpot-backend --pod penpot --env-file=./pod.env --env-file=./pod.secrets.env --volume=./assets:/opt/data/assets:U,rw --user=0:0 --requires=penpot-redis,penpot-postgres -d penpotapp/backend:latest
-podman run --replace --name=penpot-frontend --pod penpot --env-file=./pod.env --volume=./assets:/opt/data/assets:rw --requires=penpot-backend -d penpotapp/frontend:latest
+podman run --replace --name=penpot-backend --pod penpot --env-file=./pod.env --env-file=./pod.secrets.env --volume=./assets:/opt/data/assets:U,rw --user=0:0 --requires=penpot-redis,penpot-postgres -d docker.io/penpotapp/backend:latest
+podman run --replace --name=penpot-frontend --pod penpot --env-file=./pod.env --volume=./assets:/opt/data/assets:rw --requires=penpot-backend -d docker.io/penpotapp/frontend:latest
+
